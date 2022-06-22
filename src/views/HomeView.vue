c177bdff9a1b4ce3e03e75730a55edc7807ca79c
tuankiet0901 <kiet-lt@genkisystem.com>
2022-06-22 14:13:53 +0700

    Update to trigger

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Notification when deployed 1!!!" />
+    <HelloWorld msg="GitHub Actions is running!!!" />
   </div>
 </template>
 
