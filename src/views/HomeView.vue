8c5bb443dc133b0459dd07a6c840bfe9d7b74869
tuankiet0901 <kiet-lt@genkisystem.com>
2022-06-22 14:34:33 +0700

    Run Actions please

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="GitHub Actions is running!!!" />
+    <HelloWorld msg="GitHub Actions is running pleaseeeee!!!" />
   </div>
 </template>
 
